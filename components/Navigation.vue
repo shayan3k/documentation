c148a33d42cd2efcdb7728ea86d16b41d55558bf
shayan <shayan3k@gmail.com>
2023-04-11 00:15:51 +1000

    update

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,7 +1,7 @@
 <!-- Please remove this file from your project -->
 <template>
   <div
-    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
+    class="relative flex items-top justify-center min-h-screen bg-gray-800 sm:items-center sm:pt-0"
   >
     <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
       <a
@@ -47,7 +47,7 @@
         </svg>
       </a>
 
-      <div class="flex gap-2">
+      <div class="flex flex-col md:flex-row gap-2">
         <NuxtLink to="/html">
           <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
             <h2 class="text-2xl leading-7 font-semibold">HTML Documentation</h2>
